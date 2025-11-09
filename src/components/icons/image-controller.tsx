072427bb30fb92fcdeeb082ca7606663983d6c88
Rahul Sharma <rahulcloud81@gmail.com>
2025-11-09 22:48:29 +0000

    The image is broken. Url not found

--- src/components/icons/image-controller.tsx
+++ src/components/icons/image-controller.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 export default function ImageController() {
   return (
     <Image
-      src="https://fireworks.so/images/enterprise/pages/homepage-enterprise/contact-us-hero.png"
+      src="https://picsum.photos/seed/xbox-controller/400/250"
       alt="Xbox Controller"
       width={400}
       height={250}
